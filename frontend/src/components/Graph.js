b535e30e84e1ffa208dfc3563e9e852c7767d619
Devanshi Trivedi <devanshi.trivedi@sjsu.edu>
2019-04-30 23:05:14 -0700

    Modified Graph by adding data values

--- frontend/src/components/Graph.js
+++ frontend/src/components/Graph.js
@@ -10,7 +10,8 @@ class Graph extends Component {
             startValue: 0,
             windowSizeValue: 5,
             data: [],
-            date: []
+            date: [],
+            datalength: 0
         }
         this.dataSpan = this.dataSpan.bind(this);
         this.clickNext = this.clickNext.bind(this);
@@ -18,7 +19,6 @@ class Graph extends Component {
     }
 
     componentDidMount() {
-
         this.drawChart();
     }
 
@@ -53,13 +53,14 @@ class Graph extends Component {
                 console.log("The response data length is : ", res.data.length);
                 for (let i = 0; i < res.data.length; i++) {
                     this.setState({
-                        data: [...this.state.data, res.data[i].value / 100000000000],
-                        date: [...this.state.date, res.data[i].date]
+                        data: [...this.state.data, (res.data[i].value / 100000000000).toFixed(2)],
+                        date: [...this.state.date, parseInt(res.data[i].date)]
 
                     })
                 }
-                console.log("Values : ", this.state.data);
-                console.log("Date : ", this.state.date);
+                this.setState({
+                    datalength: this.state.date.length
+                })
 
                 this.displayChart();
             })
@@ -69,14 +70,20 @@ class Graph extends Component {
     }
     displayChart() {
 
-        const dataLen = this.state.data.length;
-        const width = 800 / dataLen;
-        const data = this.state.data;
+        let dataLen = this.state.data.length;
+        let width = 800 / dataLen;
+        let data = this.state.data;
+        let dates = this.state.date;
         console.log("Data before using in chart: ", data);
+        console.log("Date before using in chart: ", dates);
         console.log("width", width);
 
         d3.select(this.node).select("svg").remove();
-        const svg = d3.select(this.node).append('svg').attr("width", 900).attr("height", 300).attr("fill", "pink");
+
+        let svg = d3.select(this.node).append('svg')
+            .attr("width", 900)
+            .attr("height", 300)
+            .attr("fill", "pink");
 
         svg.selectAll("rect")
             .data(data)
@@ -86,8 +93,19 @@ class Graph extends Component {
             .attr("y", (d, i) => 300 - d)
             .attr("width", width)
             .attr("height", (d, i) => d)
-            .attr("fill", "green")
+            .attr("fill", "green");
+
+        svg.selectAll("text")
+            .data(data)
+            .enter()
+            .append("text")
+            .text((d) => d)
+            .attr("x", (d, i) => i * (width + 5))
+            .attr("y", (d, i) => 300 - d - 5)
+            .attr("fill", "black");
+
     }
+
 
     render() {
         return (
@@ -99,23 +117,26 @@ class Graph extends Component {
                 <div className="container">
                     <div className="row">
 
-                        <div className="col-sm-2">
+                        <div className="col-sm-2" style={{ "border-right": "2px solid grey" }}>
                             <div className="selectionBar">
                                 <h5>Enter the number of years(Window size) of data that you want</h5>
                                 <h6>(Range 5-20):</h6>
                                 <input onChange={this.dataSpan} type="number" name="departDate" placeholder="5" min="5" max="20" ></input>
                                 <br></br><br></br>
-                                <h6>Display previous years GDP.</h6><br></br>
-                                <button onClick={this.clickNext} type="button">Next</button>
+                                <h6>Display previous years GDP.</h6>
+                                <button onClick={this.clickNext} className="btn btn-success" type="button">Next</button>
                             </div>
                         </div>
 
                         <div className="col-sm-10">
                             <div>
-                                <div ref={(node) => this.node = node}></div>
+                                <h3>Years</h3>
+                                <h3>{this.state.date[0]} - {this.state.date[this.state.date.length - 1]}</h3>
+                                <div ref={(node) => this.node = node}></div><br></br>
                             </div>
                         </div>
                     </div>
+
                 </div>
             </div>
 
