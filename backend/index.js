61a1577a3e1881c1bd2d3fca0859c81d4634faae
Devanshi Trivedi <devanshi.trivedi@sjsu.edu>
2019-04-30 21:08:33 -0700

    Updated splicing in backend; Added Bar Graph in frontend

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,34 @@
 var express = require('express');
 var app = express();
-const fetch = require('node-fetch')
+var axios = require('axios');
+var cors = require('cors');
+const bodyParser = require('body-parser');
 let data = []
 
-app.get('/api/graph', function(req,res){
-    
-    res.json(data.slice(0,5))
+//use cors to allow cross origin resource sharing
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/graph', function (req, res) {
+    console.log("Inside api call");
+    console.log(req.query);
+    let start = parseInt(req.query.start);
+    let windowSize = parseInt(req.query.windowSize);
+    console.log(data[0].total);
+    let totalValues = data[0].total;
+    if (windowSize + start > totalValues) {
+        res.json(data[1].slice(start, totalValues))
+    } else {
+        res.json(data[1].slice(start, windowSize + start))
+    }
+
 })
 
-fetch('http://api.worldbank.org/countries/USA/indicators/NY.GDP.MKTP.CD?per_page=5000&format=json')
+axios.get('http://api.worldbank.org/countries/USA/indicators/NY.GDP.MKTP.CD?per_page=5000&format=json')
     .then(response => {
-        data = [...response.data[1]]
+        data = [...response.data]
         if (!data.length) throw Error("no data found!")
     })
     .then(_ => {
@@ -19,13 +37,3 @@ fetch('http://api.worldbank.org/countries/USA/indicators/NY.GDP.MKTP.CD?per_page
     })
     .catch(e => console.error(e));
 
-
-    // axios.get('http://api.worldbank.org/countries/USA/indicators/NY.GDP.MKTP.CD?per_page=5000&date=2001:2002&format=json').then(response => {
-    //     console.log("The response is :", response);
-    //     console.log("The response.data is :", response.data);
-    //     console.log("The response.data[0] is :", response.data[0]);
-    //     console.log("The response.data[1].length is :", response.data[1][1]);
-
-
-    //     res.send(response.data);
-    // })
--- frontend/src/components/Graph.js
+++ frontend/src/components/Graph.js
@@ -1,33 +1,126 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './graph.css';
+import * as d3 from "d3";
 
 class Graph extends Component {
-    componentDidMount(){
+    constructor(props) {
+        super(props);
+        this.state = {
+            startValue: 0,
+            windowSizeValue: 5,
+            data: [],
+            date: []
+        }
+        this.dataSpan = this.dataSpan.bind(this);
+        this.clickNext = this.clickNext.bind(this);
+        this.node = null;
+    }
+
+    componentDidMount() {
+
         this.drawChart();
     }
-    drawChart(){
-        console.log("Hi");
+
+    dataSpan = (e) => {
+        this.setState({
+            windowSizeValue: e.target.value
+        }, this.drawChart)
     }
-    render(){
-        return( 
-            
-            <div> 
-                <div class="page-header">
-                    <h3 class="text-center">GDP growth of U.S.A for the past 60 years</h3>
+    clickNext = (e) => {
+        console.log("Inside next click");
+        let inc_value = this.state.startValue;
+        inc_value++;
+        this.setState({
+            startValue: inc_value
+        }, this.drawChart)
+    }
+
+    drawChart() {
+        this.setState({
+            date: [],
+            data: []
+        })
+
+        axios.defaults.withCredentials = true;
+        console.log("Inside Draw Chart", this.state.windowSizeValue);
+        const data = {
+            start: this.state.startValue,
+            windowSize: this.state.windowSizeValue
+        }
+        axios.get(`http://localhost:3001/api/graph`, { params: data })
+            .then(res => {
+                console.log("The response data length is : ", res.data.length);
+                for (let i = 0; i < res.data.length; i++) {
+                    this.setState({
+                        data: [...this.state.data, res.data[i].value / 100000000000],
+                        date: [...this.state.date, res.data[i].date]
+
+                    })
+                }
+                console.log("Values : ", this.state.data);
+                console.log("Date : ", this.state.date);
+
+                this.displayChart();
+            })
+            .catch(err => {
+                console.log("Error in getting the data values.");
+            });
+    }
+    displayChart() {
+
+        const dataLen = this.state.data.length;
+        const width = 800 / dataLen;
+        const data = this.state.data;
+        console.log("Data before using in chart: ", data);
+        console.log("width", width);
+
+        d3.select(this.node).select("svg").remove();
+        const svg = d3.select(this.node).append('svg').attr("width", 900).attr("height", 300).attr("fill", "pink");
+
+        svg.selectAll("rect")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("x", (d, i) => i * (width + 5))
+            .attr("y", (d, i) => 300 - d)
+            .attr("width", width)
+            .attr("height", (d, i) => d)
+            .attr("fill", "green")
+    }
+
+    render() {
+        return (
+
+            <div>
+                <div className="page-header">
+                    <h3 className="text-center">GDP growth of U.S.A for the past 60 years</h3>
                 </div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-8">GRAPH DISPLAY</div>
-                        <div class="col-sm-4">
-                            <div class="selectionBar">
-                            <input onChange={this.dataSpan} type="number" name="departDate" ></input>
+                <div className="container">
+                    <div className="row">
+
+                        <div className="col-sm-2">
+                            <div className="selectionBar">
+                                <h5>Enter the number of years(Window size) of data that you want</h5>
+                                <h6>(Range 5-20):</h6>
+                                <input onChange={this.dataSpan} type="number" name="departDate" placeholder="5" min="5" max="20" ></input>
+                                <br></br><br></br>
+                                <h6>Display previous years GDP.</h6><br></br>
+                                <button onClick={this.clickNext} type="button">Next</button>
+                            </div>
+                        </div>
+
+                        <div className="col-sm-10">
+                            <div>
+                                <div ref={(node) => this.node = node}></div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            
+
         )
+
     }
 }
 export default Graph;
